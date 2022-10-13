0eadb967af20b9b13c4875227820dda932bf28c8
NikHectro <99733904+NikHectro@users.noreply.github.com>
2022-10-13 17:25:59 +0300

    Refactoring from class to hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,85 +1,66 @@
 import React from 'react';
-import { Component } from 'react';
+import { useState } from 'react';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 
-export class App extends Component {
-  static defaultProps = {
-    initialValue: 0,
+export const App = () => {
+  const initialValue = 0;
+
+  const [good, setGood] = useState(initialValue);
+  const [neutral, setNeutral] = useState(initialValue);
+  const [bad, setBad] = useState(initialValue);
+
+  const handleBtnClick = key => {
+    switch (key) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+
+      default:
+        break;
+    }
   };
 
-  state = {
-    good: this.props.initialValue,
-    neutral: this.props.initialValue,
-    bad: this.props.initialValue,
-  };
+  const countTotalFeedback = () => good + neutral + bad;
 
-  handleBtnClick = key => {
-    this.setState(prevState => ({
-      [key]: prevState[key] + 1,
-      // [event.target.name]: prevState[event.target.name] + 1,
-    }));
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    const countTotal = good + neutral + bad;
-    return countTotal;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
     return total > 0 ? Math.round((good / total) * 100) : 0;
   };
 
-  render() {
-    const keys = Object.keys(this.state);
-    const values = Object.entries(this.state);
-    const total = this.countTotalFeedback();
-    const percentage = this.countPositiveFeedbackPercentage();
+  const keys = ['good', 'neutral', 'bad'];
+  const keysArr = { good, neutral, bad };
+  const values = Object.entries(keysArr);
+  const total = countTotalFeedback();
+  const percentage = countPositiveFeedbackPercentage();
 
-    return (
-      <div>
-        <h1>Please leave feedback</h1>
-        <Section title="Feedback form:">
-          <FeedbackOptions keys={keys} handleBtnClick={this.handleBtnClick} />
-        </Section>
+  return (
+    <div>
+      <h1>Please leave feedback</h1>
+      <Section title="Feedback form:">
+        <FeedbackOptions keys={keys} handleBtnClick={handleBtnClick} />
+      </Section>
 
-        <Section title="Statistics:">
-          {total ? (
-            <Statistics
-              values={values}
-              total={total}
-              positivePercentage={percentage}
-            />
-          ) : (
-            <Notification message="There is no feedbacks" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
-
-/* {keys.map(key => (
-          <button
-            type="button"
-            key={key}
-            onClick={() => this.handleBtnClick(key)}
-          >
-            {key}
-          </button>
-        ))} */
-
-/* <button type="button" name="good" onClick={this.handleBtnClick}>
-          Good
-        </button>
-        <button type="button" name="neutral" onClick={this.handleBtnClick}>
-          Neutral
-        </button>
-        <button type="button" name="bad" onClick={this.handleBtnClick}>
-          Bad
-        </button> */
+      <Section title="Statistics:">
+        {total ? (
+          <Statistics
+            values={values}
+            total={total}
+            positivePercentage={percentage}
+          />
+        ) : (
+          <Notification message="There is no feedbacks" />
+        )}
+      </Section>
+    </div>
+  );
+};
